8b6dd473831170d062ad399851b69a06f2ae7285
muratmnz <minazzmurat@gmail.com>
2023-02-02 23:25:45 +0300

    added Intent handling between activities. edited UI

--- app/src/main/java/com/muratmnz/basicskotlin/MainActivity.kt
+++ app/src/main/java/com/muratmnz/basicskotlin/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.muratmnz.basicskotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnNext: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //initialize btnNext with view
+        btnNext = findViewById(R.id.btnNext)
+
+        //navigate between activities
+        btnNext.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/muratmnz/basicskotlin/SecondActivity.kt
+++ app/src/main/java/com/muratmnz/basicskotlin/SecondActivity.kt
@@ -1,11 +1,29 @@
 package com.muratmnz.basicskotlin
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
+
+    lateinit var btnNextPage: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        //initialize btnNext with view
+        btnNextPage = findViewById(R.id.btnNextPage)
+
+        //navigate between activities
+        btnNextPage.setOnClickListener {
+            val intentSecond = Intent(this, ThirdActivity::class.java)
+            startActivity(intentSecond)
+            finish()
+        }
+
     }
 }
