058fee1477def3a8a541a8da955846cb289be22f
muratmnz <minazzmurat@gmail.com>
2023-02-14 00:01:11 +0300

    added Navigation drawer feature inside SecondActivity.

--- app/src/main/java/com/muratmnz/basicskotlin/SecondActivity.kt
+++ app/src/main/java/com/muratmnz/basicskotlin/SecondActivity.kt
@@ -4,15 +4,18 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.muratmnz.basicskotlin.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
 
     lateinit var binding: ActivitySecondBinding
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,11 +55,59 @@ class SecondActivity : AppCompatActivity() {
             binding.linearLayout.addView(itemText)
 
         }
+        //apply binding with function
+        binding.apply {
+            toggle = ActionBarDrawerToggle(
+                this@SecondActivity,
+                drawerLayout,
+                R.string.open,
+                R.string.close
+            )
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            //navigation drawer includes menu items.
+            binding.navView.setNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.firstItem -> {
+                        Toast.makeText(
+                            this@SecondActivity,
+                            "First Item Clicked",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    R.id.secondtItem -> {
+                        Toast.makeText(
+                            this@SecondActivity,
+                            "Second Item Clicked",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    R.id.thirdItem -> {
+                        Toast.makeText(
+                            this@SecondActivity,
+                            "third Item Clicked",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+                true
+            }
+        }
+
     }
 
-    //Back button
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return true
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            true
+        }
+        return super.onOptionsItemSelected(item)
     }
+
+//    //Back button
+//    override fun onSupportNavigateUp(): Boolean {
+//        onBackPressed()
+//        return true
+//    }
 }
