aeec0eb377b6cd89f2782a957211db38d1802016
muratmnz <minazzmurat@gmail.com>
2023-02-14 22:42:51 +0300

    added Spinner menu inside ThirdActivity.

--- app/src/main/java/com/muratmnz/basicskotlin/ThirdActivity.kt
+++ app/src/main/java/com/muratmnz/basicskotlin/ThirdActivity.kt
@@ -1,9 +1,12 @@
 package com.muratmnz.basicskotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebViewClient
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.muratmnz.basicskotlin.databinding.ActivitySecondBinding
 import com.muratmnz.basicskotlin.databinding.ActivityThirdBinding
 import kotlinx.android.synthetic.main.activity_third.*
@@ -11,6 +14,10 @@ import kotlinx.android.synthetic.main.activity_third.*
 class ThirdActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityThirdBinding
+
+    //Mutable list - changeble list.
+    val pageList: MutableList<String> = mutableListOf("1", "2", "3", "4")
+    private lateinit var category: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +30,24 @@ class ThirdActivity : AppCompatActivity() {
 
         binding.webView.loadUrl("https://developer.android.com/docs")
 
+        binding.btnBack.setOnClickListener {
+            val intentThird = Intent(this,SecondActivity::class.java)
+            startActivity(intentThird)
+            finish()
+        }
+
+
         var settings: WebSettings = binding.webView.settings
         settings.javaScriptEnabled = true
         settings.setSupportZoom(true)
+
+        category = binding.spCategory
+
+        //adapter connect with list
+        val adapter =
+            ArrayAdapter(this@ThirdActivity, android.R.layout.simple_spinner_item, pageList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        category.adapter = adapter
 
     }
 }
